23c8f6f7fd2ea7b195c9d14ce36ab5c212e824b7
sahilgupta5 <sahilgupta5@hotmail.com>
2016-10-10 22:26:03 -0400

    CARMAISE configuration files for mysql server

--- configure-mysql-server.sh
+++ configure-mysql-server.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
 PATH=$1
 /usr/bin/mysqld_safe &
 sleep 5
-
-cd $PATH;
-for f in *.sql
+for f in $PATH/*.sql
 do
     mysql -user=FoodLineTestUser --password='foodline123!@#' -h localhost FoodLineDB < $f
 done
