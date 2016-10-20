f0025c2e810e29653d23f80cb5139115e7a32aea
sahilgupta5 <sahilgupta5@hotmail.com>
2016-10-20 06:27:25 -0400

    Updating installing docker script for Ubuntu

--- installDocker.sh
+++ installDocker.sh
@@ -10,7 +10,21 @@ fi
 sleep 5;
 done
 
-sudo apt-get install -y docker
+#Ref: https://docs.docker.com/engine/installation/linux/ubuntulinux/
+
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+#Get the list of sources for docker
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee -a /etc/apt/sources.list.d/docker.list
+
+#Get recommended packages
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+#Run the update to make sure repo list is updated with docker source repos
+sudo apt-get --yes update
+
+sudo apt-get install -y docker-engine
 sudo service docker start
 
 sudo docker run -d -p 80:80 -p 3306:3306 -p 443:443 sahilgupta/carmaise
